b3ae1973bc52f118087e24fd6866f5c42303c466
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-07-31 12:20:30 +0200

    initial commit

---
+++ src/blog/views/common.clj
@@ -0,0 +1,12 @@
+(ns blog.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "blog"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
