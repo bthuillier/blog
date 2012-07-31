456235cde63b5a5b1aba284798a58a433bf4b6d4
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-07-31 20:15:34 +0200

    add a navbar to the layout

--- src/blog/views/common.clj
+++ src/blog/views/common.clj
@@ -2,6 +2,13 @@
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-js include-css html5]]))
 
+(defpartial navbar []
+[:div { :class "navbar navbar-fixed-top" }
+				[:div.navbar-inner
+		 		 [:div.container-fluid]]]
+	)
+
+
 (defpartial layout [& content]
             (html5
               [:head
@@ -9,9 +16,11 @@
                (include-css "/css/main.css")
                [:meta { :name "viewport" :content "width=device-width, initial-scale=1.0" }]]
               [:body
+			   (navbar)
                [:div#main.container-fluid
                 [:div.row-fluid
                  [:div.span12 content
                ]]]
                (include-js "/js/jquery.min.js")
                (include-js "/js/bootstrap.min.js")]))
+
