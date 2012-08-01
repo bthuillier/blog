622bb53f876dd18a4aee0a8181ba4a32047e39ae
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-08-02 00:56:05 +0200

    add some function to easily manage css and js

--- src/blog/views/common.clj
+++ src/blog/views/common.clj
@@ -3,11 +3,20 @@
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-js include-css html5]]))
 
+(defn include-javascripts[libs]
+  (map include-js libs))
+
+(defn include-stylesheets[libs]
+  (map include-css libs))
+
+(def base-javascripts["/js/jquery.min.js" "/js/bootstrap.min.js"])
+(def base-stylesheets["/css/main.css"])
+
 (defpartial layout [nav content]
             (html5
               [:head
                [:title "blog"]
-               (include-css "/css/main.css")
+               (include-stylesheets base-stylesheets)
                [:meta { :name "viewport" :content "width=device-width, initial-scale=1.0" }]]
               [:body
 			         (nav)
@@ -15,8 +24,9 @@
                 [:div.row-fluid
                  [:div.span12 content
                ]]]
-               (include-js "/js/jquery.min.js")
-               (include-js "/js/bootstrap.min.js")]))
+               (include-javascripts base-javascripts)]))
+
+
 
 (defpartial default-layout [ & content]
   (layout navbar/generate-default content))
